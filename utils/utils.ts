beb95fe05ab17a3c10a709ffb0005044eac8fd7f
Dunisani Makondo <dunisani7@gmail.com>
2025-07-25 10:02:48 +0200

    fixed the utils type

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,6 +1,11 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs) {
-  return twMerge(clsx(inputs));
+/**
+ * Combines class names using clsx and merges Tailwind classes intelligently.
+ * @param inputs Any number of class name values.
+ * @returns A single merged className string.
+ */
+export function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(...inputs));
 }
